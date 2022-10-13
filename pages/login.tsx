947be44938ee9f2436a0f62b83a57dc8e18695e7
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-13 11:53:36 -0300

    Deploy

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,48 @@
+import { Typography } from '@mui/material';
 import React from 'react'
+import Link from 'next/link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+type CopyProps = {
+    site: string;
+    sx?:object;
+}
+
+function Copyright(props:CopyProps){
+    return (
+        <Typography>
+            {'Copyright © '}
+            <Link color="inherit" href="https://www.avanade.com.br/">
+                {props.site}
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
+
+const theme = createTheme();
 
 export default function LoginPage() {
   return (
-    <div>login</div>
+    <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+             <Box sx={{mt:8, display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <Box component="form" onSubmit={(e)=>{console.log('enviou')}}>
+    <TextField margin="normal" required fullWidth id="email" label="Digite o e-mail" name="email" autoComplete="email" autoFocus/>
+    <TextField margin="normal"  required fullWidth id="password" type="password" label="Digite a senha" name="password" autoComplete="current-password" autoFocus/>
+                </Box>
+             </Box>
+            <Copyright site="www.avanade.com.br" sx={{mt:8, mb: 4}} />
+        </Container>
+    </ThemeProvider>
   )
 }
