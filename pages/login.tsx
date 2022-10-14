fb86fa794db9ca13115eba3c53b097de5da85e1f
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-14 11:50:59 -0300

    Deploy

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,21 @@
 import React, {useState, useEffect, FormEvent} from 'react'
-import { Typography, Container, CssBaseline,Box,TextField, Checkbox, Button, FormControlLabel } from '@mui/material';
+import { Typography, Container, CssBaseline,Box,TextField,
+     Checkbox, Button, FormControlLabel, Stack, Snackbar } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 import Link from 'next/link';
+
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+
+
+
 type CopyProps = {
     site: string;
     sx?:object;
@@ -28,6 +42,11 @@ export default function LoginPage() {
 const [empresa, setEmpresa] = useState<string>('');
 const [nome, setNome] = useState('');
 const [contador, setContador] = useState<number>(5);
+const [error, setError] = useState<boolean>(false);
+const [errorMessage, setErrorMessage] = useState<string>('');
+const [email, setEmail] = useState<string | undefined | null | FormDataEntryValue>('');
+const [password, setPassword] = useState<string | undefined | null | FormDataEntryValue>('');
+const [open, setOpen] = useState<boolean>(false);
 
 // A primeira vez após carregar a página e após o render
 // Executa também a cada alteração de estado
@@ -49,12 +68,35 @@ useEffect(()=>{
 },[nome])
 
 
+useEffect(()=>{
+
+    if(password && password.length < 6){
+        setError(true);
+        setErrorMessage('A senha deve ter no mínimo 6 caracteres');
+    }else if(password) {
+        setError(false);
+        setErrorMessage('');
+        //enviar o formulário para o servidor........
+        //deu certo... vamos criar o snackbar...
+        setOpen(true);
+    }
+
+},[password]);
+
+const handleClose = ()=>{
+    setOpen(false);
+}
+
 const handleSubmit = (event: FormEvent<HTMLFormElement>)=>{
     // Previne o comportamento padrão do formulário, que seria recarregar a página.
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
-    console.log(data);
+    console.log(data.get('email'));
+    console.log(data.get('password'));
+
+    setEmail(data.get('email'));
+    setPassword(data.get('password'));
 }
 
 
@@ -65,6 +107,14 @@ const handleSubmit = (event: FormEvent<HTMLFormElement>)=>{
     <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs">
             <CssBaseline />
+                <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    Usuário autenticado com sucesso... aguarde...
+                    </Alert>
+                </Snackbar>
+
+
+
              <Box sx={{mt:8, display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
                 <Typography component="h1" variant="h5">
                     Login
@@ -79,6 +129,9 @@ const handleSubmit = (event: FormEvent<HTMLFormElement>)=>{
     <TextField margin="normal"  required fullWidth id="password" type="password" label="Digite a senha" name="password" autoComplete="current-password" autoFocus/>
                     <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Lembrar de mim" />
                     <Button type="submit" fullWidth variant="contained" sx={{mt:3, mb:2}}>Entrar</Button>
+
+                    {error && <Typography color="error">{errorMessage}</Typography>}
+
                 </Box>
              </Box>
             <Copyright site="www.avanade.com.br" sx={{mt:8, mb: 4}} />
